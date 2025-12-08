9b9b776a39a20c6ca9ba615fddafbc76ff6dbd82
剛 増谷 <K024G0075@g.neec.ac.jp>
2025-12-08 21:45:35 +0900

    衝撃波のモデル完成

--- project/magosuya/utility/camera/LookAtCamera.cpp
+++ project/magosuya/utility/camera/LookAtCamera.cpp
@@ -1,1 +1,22 @@
 #include "LookAtCamera.h"
+#include "MathFunction.h"
+
+LookAtCamera::LookAtCamera (InputManager* inputManager) {
+
+}
+
+LookAtCamera::~LookAtCamera () {
+
+}
+
+void LookAtCamera::Initialize (const Transform& transform) {
+
+}
+
+void LookAtCamera::Update () {
+
+}
+
+void LookAtCamera::ImGui () {
+
+}
