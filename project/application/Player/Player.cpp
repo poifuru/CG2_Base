e49de99779f58717c0541b1dd051b68f2a1455a9
masuya_gou <k024g0075@g.neec.ac.jp>
2025-12-09 10:30:54 +0900

    プレイヤーの影修正

--- project/application/Player/Player.cpp
+++ project/application/Player/Player.cpp
@@ -8,142 +8,141 @@ Player::~Player () {
 	delete state_;
 }
 
-void Player::Initialize()
-{
-	ModelManager::GetInstance()->LoadModelData ("Resources/player", "player");
-	obj_ = std::make_unique<Model>(DxCommon::GetInstance());
-	obj_->SetModelData("player");
-	obj_->SetTexture("player");
-	obj_->Initialize();
-	obj_->SetPosition({0.0f,0.0f,-5.0f});
+void Player::Initialize () {
+	ModelManager::GetInstance ()->LoadModelData ("Resources/player", "player");
+	obj_ = std::make_unique<Model> (DxCommon::GetInstance ());
+	obj_->SetModelData ("player");
+	obj_->SetTexture ("player");
+	obj_->Initialize ();
+	obj_->SetPosition ({ 0.0f,0.0f,-5.0f });
 
 	// 始まりのState
-	state_ = new PlayerStopState();
-	state_->SetPlayer(this);
+	state_ = new PlayerStopState ();
+	state_->SetPlayer (this);
 
-	attackColliderObj_ = std::make_unique<Model>(DxCommon::GetInstance());
-	attackColliderObj_->SetModelData("teapot");
-	attackColliderObj_->SetTexture("teapot");
-	attackColliderObj_->Initialize();
+	attackColliderObj_ = std::make_unique<Model> (DxCommon::GetInstance ());
+	attackColliderObj_->SetModelData ("teapot");
+	attackColliderObj_->SetTexture ("teapot");
+	attackColliderObj_->Initialize ();
 
-	playerColliderObj_ = std::make_unique<Model>(dxCommon_);
-	playerColliderObj_->SetModelData("teapot");
-	playerColliderObj_->SetTexture("teapot");
-	playerColliderObj_->Initialize();
+	playerColliderObj_ = std::make_unique<Model> (dxCommon_);
+	playerColliderObj_->SetModelData ("slipDamage");
+	playerColliderObj_->SetTexture ("slipDamage");
+	playerColliderObj_->Initialize ();
 
 	// [ 設定 ]
 	// 1, Player
-	playerCollider_ = std::make_unique<PlayerBodyCollider>(this);
+	playerCollider_ = std::make_unique<PlayerBodyCollider> (this);
 	// 2, Attack
-	attackCollider_ = std::make_unique<AttackCollider>();
-	EnableHitBox(false, obj_->GetTransform().translate);
+	attackCollider_ = std::make_unique<AttackCollider> ();
+	EnableHitBox (false, obj_->GetTransform ().translate);
 }
 
-void Player::Update(Matrix4x4* m)
-{
+void Player::Update (Matrix4x4* m) {
 	// 毎フレーム初期化する処理↓↓↓
 	move_ = { 0.0f,0.0f,0.0f };
 
 	// 毎フレーム初期化する処理↑↑↑
 
-	ApplyPhysics();
+	ApplyPhysics ();
 
 	if (state_) {
-		state_->Update();
+		state_->Update ();
 	}
 
-	UpdateStaminaRecovery();
-	
+	UpdateStaminaRecovery ();
+
 	// カメラに対してにする処理
 	//move_ = move_.z * CameraSystem::GetInstance()->GetActiveCamera()->zAxis_ + move_.x * CameraSystem::GetInstance()->GetActiveCamera()->xAxis_;
 	// = Normalize(move_);
 	if (move_.x != 0.0f || move_.y != 0.0f || move_.z != 0.0f) {
-		move_ = Math::Normalize(move_);
+		move_ = Math::Normalize (move_);
 		direction_.x = move_.x;
 		direction_.z = move_.z;
 	}
 	move_.y = 1.0f;
 
-	RotateToMoveDirection();
+	RotateToMoveDirection ();
 
-	Vector3 pos = obj_->GetTransform().translate;
+	Vector3 pos = obj_->GetTransform ().translate;
 	pos.x += move_.x * speed_ * speedMultiplier_;
 	pos.y += move_.y * verticalVelocity_;
 	pos.z += move_.z * speed_ * speedMultiplier_;
-	obj_->SetTransform({ obj_->GetTransform().scale,obj_->GetTransform().rotate,pos});
+	obj_->SetTransform ({ obj_->GetTransform ().scale,obj_->GetTransform ().rotate,{pos.x, pos.y - 1.0f, pos.z} });
 
-	playerColliderObj_->SetTransform({ {playerCollider_->GetRadius(),0.1f,playerCollider_->GetRadius()},{0.0f,0.0f,0.0f},
-		{playerCollider_->GetWorldPosition().x,
-		playerCollider_->GetWorldPosition().y - 0.5f,
-		playerCollider_->GetWorldPosition().z} });
+	playerColliderObj_->SetTransform ({ {playerCollider_->GetRadius () * 0.7f,0.1f,playerCollider_->GetRadius () * 0.7f},{0.0f,0.0f,0.0f},
+		{playerCollider_->GetWorldPosition ().x,
+		playerCollider_->GetWorldPosition ().y + 0.05f,
+		playerCollider_->GetWorldPosition ().z} });
 
-	attackColliderObj_->SetTransform({ {attackCollider_->GetRadius(),0.1f,attackCollider_->GetRadius()},{0.0f,0.0f,0.0f},
-		{attackCollider_->GetWorldPosition().x,
-		attackCollider_->GetWorldPosition().y - 0.5f,
-		attackCollider_->GetWorldPosition().z} });
+	playerColliderObj_->SetColor ({ 0.0f, 0.0f, 0.0f, 1.0f });
 
-	obj_->Update(m);
-	playerColliderObj_->Update(m);
-	attackColliderObj_->Update(m);
+	attackColliderObj_->SetTransform ({ {attackCollider_->GetRadius (),0.1f,attackCollider_->GetRadius ()},{0.0f,0.0f,0.0f},
+		{attackCollider_->GetWorldPosition ().x,
+		attackCollider_->GetWorldPosition ().y - 0.5f,
+		attackCollider_->GetWorldPosition ().z} });
+
+	obj_->Update (m);
+	playerColliderObj_->Update (m);
+	attackColliderObj_->Update (m);
 #ifdef _DEBUG
-	ImGui::Begin("Player");
-	ImGui::SliderFloat3("Direction", &direction_.x,0.0f,0.0f);
-	ImGui::DragFloat3("pos", &pos.x);
-	ImGui::DragFloat("HP", &hp_);
-	ImGui::DragFloat("Normal Speed", &speed_,0.01f);
-	ImGui::End();
-	obj_->ImGui("player");
+	ImGui::Begin ("Player");
+	ImGui::SliderFloat3 ("Direction", &direction_.x, 0.0f, 0.0f);
+	ImGui::DragFloat3 ("pos", &pos.x);
+	ImGui::DragFloat ("HP", &hp_);
+	ImGui::DragFloat ("Normal Speed", &speed_, 0.01f);
+	ImGui::End ();
+	obj_->ImGui ("player");
 #endif//_DEBUG
 }
 
-void Player::Draw()
-{
+void Player::Draw () {
 	// 描画処理
 	// [ キャラ ]
-	obj_->Draw();
+	obj_->Draw ();
 	// [ プレイヤー自身の当たり判定 ]
-	playerColliderObj_->Draw();
+	playerColliderObj_->Draw ();
 	if (isAttackViewFlag_) {
 		// [ 攻撃の当たり判定 ]
-		attackColliderObj_->Draw();
+		attackColliderObj_->Draw ();
 	}
 }
 
-void Player::ChangeState(PlayerState* newState) {
+void Player::ChangeState (PlayerState* newState) {
 	if (state_) {
-		state_->Exit();
+		state_->Exit ();
 
 		delete state_;
 		state_ = nullptr;
 	}
 	state_ = newState;
-	state_->SetPlayer(this);
-	state_->Initialize();
+	state_->SetPlayer (this);
+	state_->Initialize ();
 }
 
 // ------------------------------
 // AttackColliderの設定
 // ------------------------------
 
-void Player::EnableHitBox(bool enable, const Vector3& worldPos) {
+void Player::EnableHitBox (bool enable, const Vector3& worldPos) {
 	if (enable) {
 		// 攻撃判定の位置を更新
-		Matrix4x4 w = Math::MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, worldPos);
-		Matrix4x4 pW = Math::MakeAffineMatrix({ obj_->GetTransform().scale }, { obj_->GetTransform().rotate }, { obj_->GetTransform().translate });
+		Matrix4x4 w = Math::MakeAffineMatrix ({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, worldPos);
+		Matrix4x4 pW = Math::MakeAffineMatrix ({ obj_->GetTransform ().scale }, { obj_->GetTransform ().rotate }, { obj_->GetTransform ().translate });
 		w = w * pW;
 
-		attackCollider_->SetWorldPosition({w.m[3][0],w.m[3][1] ,w.m[3][2] });
+		attackCollider_->SetWorldPosition ({ w.m[3][0],w.m[3][1] ,w.m[3][2] });
 
 		// 攻撃判定をアクティブ化
-		attackCollider_->SetMyType(COL_Player_Attack);
+		attackCollider_->SetMyType (COL_Player_Attack);
 
 		// 相手のマスクも設定
-		attackCollider_->SetYourType(COL_Enemy | COL_Enemy_Attack | COL_Boss);
+		attackCollider_->SetYourType (COL_Enemy | COL_Enemy_Attack | COL_Boss);
 	}
 	else {
 		// 攻撃判定を非アクティブにする
-		attackCollider_->SetMyType(COL_None);
-		attackCollider_->SetYourType(COL_None);
+		attackCollider_->SetMyType (COL_None);
+		attackCollider_->SetYourType (COL_None);
 	}
 
 }
@@ -152,17 +151,17 @@ void Player::EnableHitBox(bool enable, const Vector3& worldPos) {
 // 物理的な処理
 // ------------------------------
 
-void Player::ApplyPhysics() {
+void Player::ApplyPhysics () {
 	if (isOnGround_ == false) {
 		verticalVelocity_ -= gravity_;
 	}
 
 	// 一旦地面との当たり判定（地面を０とする）
-	if (obj_->GetTransform().translate.y <= 0.0f) {
+	if (obj_->GetTransform ().translate.y <= 0.0f) {
 		isOnGround_ = true;
-		Vector3 pos = obj_->GetTransform().translate;
+		Vector3 pos = obj_->GetTransform ().translate;
 		pos.y = 0.0f;
-		obj_->SetTransform({ obj_->GetTransform().scale,obj_->GetTransform().rotate,pos });
+		obj_->SetTransform ({ obj_->GetTransform ().scale,obj_->GetTransform ().rotate,pos });
 	}
 
 	if (isOnGround_ == true) {
@@ -174,7 +173,7 @@ void Player::ApplyPhysics() {
 // スタミナについて
 // ------------------------------
 
-void Player::UpdateStaminaRecovery() {
+void Player::UpdateStaminaRecovery () {
 	if (staminaRecoveryBlockers_ > 0) {
 		// Blockerが１以上なら回復しない
 		return;
@@ -191,32 +190,32 @@ void Player::UpdateStaminaRecovery() {
 // 旋回しょり
 // ------------------------------
 
-void Player::RotateToMoveDirection() {
+void Player::RotateToMoveDirection () {
 	// 移動処理がなければスキップ
 	if (move_.x == 0.0f && move_.z == 0.0f) {
 		// 入力が無かった
 		return;
 	}
 
-	float targetAngle = std::atan2(move_.x, move_.z);
+	float targetAngle = std::atan2 (move_.x, move_.z);
 
-	targetAngle = std::fmod(targetAngle, Math::Deg2Rad(360));
+	targetAngle = std::fmod (targetAngle, Math::Deg2Rad (360));
 
-	if (targetAngle >= Math::Deg2Rad(180)) {
-		targetAngle -= Math::Deg2Rad(360);
+	if (targetAngle >= Math::Deg2Rad (180)) {
+		targetAngle -= Math::Deg2Rad (360);
 	}
-	else if (targetAngle <= Math::Deg2Rad(-180)) {
-		targetAngle += Math::Deg2Rad(360);
+	else if (targetAngle <= Math::Deg2Rad (-180)) {
+		targetAngle += Math::Deg2Rad (360);
 	}
 
 
-	Vector3 rot = obj_->GetTransform().rotate;
-	rot.y = 0.0f * obj_->GetTransform().rotate.y + (1.0f - 0.0f) * targetAngle;
-	obj_->SetTransform({
-		obj_->GetTransform().scale,
+	Vector3 rot = obj_->GetTransform ().rotate;
+	rot.y = 0.0f * obj_->GetTransform ().rotate.y + (1.0f - 0.0f) * targetAngle;
+	obj_->SetTransform ({
+		obj_->GetTransform ().scale,
 		rot,
-		obj_->GetTransform().translate
-		});
+		obj_->GetTransform ().translate
+						});
 	/*Quaternion targetQuaternion = Quaternion::MakeRotateAxisAngleQuaternion({ 0.0f,1.0f,0.0f }, targetAngle);
 
 	Quaternion currentQuaternion = obj_->worldTransform_.get_.Quaternion();
@@ -224,7 +223,7 @@ void Player::RotateToMoveDirection() {
 	obj_->worldTransform_.set_.Quaternion(currentQuaternion);*/
 }
 
-Vector3 Player::GetForwardVector() {
+Vector3 Player::GetForwardVector () {
 	return (direction_);
 }
 
@@ -232,9 +231,9 @@ Vector3 Player::GetForwardVector() {
 // 攻撃を喰らった時の処理
 // ---------------------------
 
-void Player::TakeDamage(float damage) {
+void Player::TakeDamage (float damage) {
 	// 1. 死亡状態、または無敵時間中の場合は処理をスキップ
-	if (IsDead() || IsInvulnerable()) {
+	if (IsDead () || IsInvulnerable ()) {
 		return;
 	}
 
@@ -245,19 +244,19 @@ void Player::TakeDamage(float damage) {
 	}
 
 	// 3. 死亡判定
-	if (IsDead()) {
+	if (IsDead ()) {
 		// HPが0になったら死亡 State へ遷移
-		ChangeState(new PlayerDeathState());
+		ChangeState (new PlayerDeathState ());
 
 		// 本体Colliderを無効化（死亡後に当たり判定を残さないため）
-		playerCollider_->SetMyType(COL_None);
+		playerCollider_->SetMyType (COL_None);
 	}
 	else {
 		// HPが残っている場合は被ダメージ State へ遷移
-		ChangeState(new PlayerHurtState());
+		ChangeState (new PlayerHurtState ());
 
 		// 無敵時間を設定する (HurtState側でリセットする)
-		SetInvulnerable(true);
+		SetInvulnerable (true);
 	}
 }
 
@@ -265,6 +264,6 @@ void Player::TakeDamage(float damage) {
 // Attack Collider
 // --------------------------
 
-void Player::AddAttackColliderType(uint32_t type) {
-	attackCollider_->SetMyType(attackCollider_->GetMyType() | type);
+void Player::AddAttackColliderType (uint32_t type) {
+	attackCollider_->SetMyType (attackCollider_->GetMyType () | type);
 }
