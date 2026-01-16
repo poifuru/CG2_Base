e57e6130f54b9192e9b9f5d06fdb2ae86bec90bf
poifuru <mocchi0927kinakokuromitu@gmail.com>
2026-01-16 09:30:50 +0900

    オブジェクト基底クラス作り途中

---
+++ project/magosuya/object/3d/BaseRenderer.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Windows.h>
+#include <wrl.h>
+using namespace Microsoft::WRL;
+#include <d3d12.h>
+#include <vector>
+
+struct RenderData {
+	//頂点
+
+
+	//インデックス
+
+
+	//
+};
+
+class BaseRenderer {
+};
