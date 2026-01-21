a5b8982508452b5975510262be3974d03cb52f60
poifuru <mocchi0927kinakokuromitu@gmail.com>
2026-01-21 15:00:45 +0900

    assimpでのobj読み込み完了

--- project/magosuya/object/3d/ModelManager.cpp
+++ project/magosuya/object/3d/ModelManager.cpp
@@ -1,5 +1,6 @@
 #include "ModelManager.h"
 #include <sstream>
+#include <cassert>
 #include <map>
 #include "MagosuyaEngine.h"
 #include "DxCommon.h"
@@ -111,110 +112,66 @@ ModelData ModelManager::LoadObjFile (const std::string& directoryPath, const std
 	textureManager_->LoadTexture (directoryPath + "/" + id + ".png", id);
 
 	//必要になる変数宣言
-	ModelData modelData;			//構築するModelData
-	std::vector<Vector4> positions;	//位置
-	std::vector<Vector3> normals;	//法線
-	std::vector<Vector2> texcoords;	//テクスチャ座標
-	std::string line;				//ファイルから読んだ１行を格納するもの
-	// 複合インデックス文字列 ("v/vt/vn") をキーとし、ユニーク頂点リストのインデックスを値とする
-	std::map<std::string, uint32_t> uniqueVertices;
+	ModelData modelData;	//構築するModelData
 
-	//あしんぽ
+	//assimpで読み込み
 	Assimp::Importer importer;
-	std::string filePath = directoryPath + "/" + id + ".png";
+	std::string filePath = directoryPath + "/" + id + ".obj";
+	const aiScene* scene = importer.ReadFile(
+		filePath.c_str(),
+		aiProcess_FlipWindingOrder | 
+		aiProcess_FlipUVs | 
+		aiProcess_JoinIdenticalVertices		//インデックス描画対応用
+	);
+	assert(scene->HasMeshes());	//メッシュが無ければ対応しない
 
-	//ファイルを開く
-	std::ifstream file (directoryPath + "/" + id + ".obj");	//ファイルを開く
-	assert (file.is_open ());	//とりあえず開けなかったら止める
+	//Meshを解析する
+	for(uint32_t meshIndex = 0; meshIndex < scene->mNumMeshes; ++meshIndex) {
+		aiMesh* mesh = scene->mMeshes[meshIndex];
+		assert(mesh->HasNormals());
+		assert(mesh->HasTextureCoords(0));
 
-	//実際にファイルを読み、ModelDataを構築していく
-	while (std::getline (file, line)) {
-		std::string identifier;
-		std::istringstream s (line);
-		s >> identifier;	//先頭の識別子を読む
+		//vertexの解析
+		for(uint32_t i = 0; i < mesh->mNumVertices; ++i) {
+			aiVector3D& position = mesh->mVertices[i];
+			aiVector3D& normal = mesh->mNormals[i];
+			aiVector3D& texcoord = mesh->mTextureCoords[0][i];
 
-		//identifierに応じた処理
-		if (identifier == "v") {
-			Vector4 position;
-			s >> position.x >> position.y >> position.z;
-			position.x *= -1.0f;
-			if (inversion) {
-				position.x *= -1.0f;
-			}
-			position.w = 1.0f;
-			positions.push_back (position);
+			VertexData vertex;
+			//aiProcess_MakeLeftHandedはz*-1	で、右手->左手に変換するので手動で対処
+			vertex.position = { -position.x, position.y, position.z, 1.0f };
+			vertex.normal = { -normal.x, normal.y, normal.z };
+			vertex.texcoord = { texcoord.x, texcoord.y };
+			modelData.vertices.push_back(vertex);
 		}
-		else if (identifier == "vt") {
-			Vector2 texcoord;
-			s >> texcoord.x >> texcoord.y;
-			texcoord.y = 1.0f - texcoord.y;
-			texcoords.push_back (texcoord);
-		}
-		else if (identifier == "vn") {
-			Vector3 normal;
-			s >> normal.x >> normal.y >> normal.z;
-			normal.x *= -1.0f;
-			if (inversion) {
-				normal.x *= -1.0f;
-			}
-			normals.push_back (normal);
-		}
-		else if (identifier == "f") {
-			//面は三角形限定。その他は未対応
-			for (int32_t faceVertex = 0; faceVertex < 3; ++faceVertex) {
-				std::string vertexDefinition;
-				s >> vertexDefinition;
 
-				// この複合インデックスが既出かチェック
-				if (uniqueVertices.count (vertexDefinition)) {
-					// 既出の場合: ユニーク頂点リストの既存のインデックスを取得
-					uint32_t index = uniqueVertices.at (vertexDefinition);
-					// 取得したインデックスを indices 配列に追加
-					modelData.indices.push_back (index);
-				}
-				else {
-					// 未出の場合: 新しいユニーク頂点として登録
-					//複合インデックスを分解して、個別のインデックスを取得 (ここまでは既存コードと同じ)
-					std::istringstream v (vertexDefinition);
-					uint32_t elementIndices[3];
-					for (int32_t element = 0; element < 3; ++element) {
-						std::string indexStr;
-						std::getline (v, indexStr, '/');
-						elementIndices[element] = std::stoi (indexStr);
-					}
+		//Faceを解析する
+		for(uint32_t faceIndex = 0; faceIndex < mesh->mNumFaces; ++faceIndex) {
+			aiFace& face = mesh->mFaces[faceIndex];
+			assert(face.mNumIndices == 3);	//三角形のみサポート
 
-					//新しいVertexDataを構築
-					Vector4 position = positions[elementIndices[0] - 1];
-					Vector2 texcoord = texcoords[elementIndices[1] - 1];
-					Vector3 normal = normals[elementIndices[2] - 1];
-					VertexData newVertex = { position, texcoord, normal };
-
-					//modelData.verticesに追加し、そのインデックスを記録
-					uint32_t newIndex = static_cast<uint32_t>(modelData.vertices.size ());
-					modelData.vertices.push_back (newVertex);
-
-					//マップに登録
-					uniqueVertices[vertexDefinition] = newIndex;
-
-					//indices にも新しいインデックスを追加
-					modelData.indices.push_back (newIndex);
-				}
+			//indexの解析
+			for(uint32_t i = 0; i < face.mNumIndices; ++i) {
+				modelData.indices.push_back(face.mIndices[i]);
 			}
 		}
-		else if (identifier == "mtllib") {
-			//materialTemplateLibraryファイルの名前を取得する
-			std::string materialFilename;
-			s >> materialFilename;
-			//基本にobjファイルと同一階層にmtlは存在させるので、ディレクトリ名とファイル名を渡す
-			modelData.material = LoadMaterialTemplateFile (directoryPath, materialFilename);
+	}
+
+	//materialの解析
+	for(uint32_t materialIndex = 0; materialIndex < scene->mNumMaterials; ++materialIndex) {
+		aiMaterial* material = scene->mMaterials[materialIndex];
+		if(material->GetTextureCount(aiTextureType_DIFFUSE) != 0) {
+			aiString textureFilePath;
+			material->GetTexture(aiTextureType_DIFFUSE, 0, &textureFilePath);
+			modelData.material.textureFilePath = directoryPath + "/" + textureFilePath.C_Str();
 		}
 	}
 
 	// 頂点数を取得 (重複が排除されたユニークな数になっているはず)
-	modelData.vertexCount = static_cast<uint32_t>(modelData.vertices.size ());
+	modelData.vertexCount = static_cast<uint32_t>(modelData.vertices.size());
 
 	// インデックス数を取得
-	modelData.indexCount = static_cast<uint32_t>(modelData.indices.size ());
+	modelData.indexCount = static_cast<uint32_t>(modelData.indices.size());
 
 	// ModelDataを返す
 	return modelData;
