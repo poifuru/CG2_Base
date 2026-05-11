2b930eccd170c68e855c3bd0b3b5d259ab5effc7
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-05-11 21:55:12 +0900

    TL1_1_3完了

--- project/tools/level_editor.py
+++ project/tools/level_editor.py
@@ -1,25 +1,58 @@
 import bpy
 
-#ブレンダーに登録するアドオン情報
+# ブレンダーに登録するアドオン情報
 bl_info = {
     "name": "レベルエディタ",
     "author": "Go Masuya",
     "version": (1, 0),
     "blender": (4, 4, 0),
-    "location": "",
+    "location": "Top Bar > Level Editor",
     "description": "レベルエディタ",
-    "warning": "",
-    "wiki_url": "",
-    "tracker_url": "",
     "category": "Object"
 }
 
+# メニューの中身を定義するクラス
+class LEVEL_EDITOR_MT_main_menu(bpy.types.Menu):
+    bl_label = "My menu"
+    bl_idname = "LEVEL_EDITOR_MT_main_menu"
+    bl_desctiption = "拡張メニュー by" + bl_info["author"]
+
+    # プルダウンメニューの中身
+    def draw(self, context):
+        layout = self.layout
+        # ここにボタンを追加していく
+        layout.operator("wm.url_open", text = "公式ドキュメントを開く").url = "https://example.com"
+
+        layout.separator()
+        layout.operator("wm.url_open", text = "チュートリアル")
+
+    def submenu(self, context):
+        # 自分の bl_idname を指定してメニューとして登録する
+        self.layout.menu(LEVEL_EDITOR_MT_main_menu.bl_idname)
+
+# 登録するクラスのリスト
+classes = (
+    LEVEL_EDITOR_MT_main_menu,
+)
+
 #アドオン有効時コールバック
 def register():
+    # クラスの登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    # トップバーのメニュー(TOPBAR_MT_editor_menus)に描画関数を追加する
+    bpy.types.TOPBAR_MT_editor_menus.append(LEVEL_EDITOR_MT_main_menu.submenu)
     print("レベルエディタが有効化されました。")
 
 #アドオン無効時コールバック
 def unregister():
+    # 追加した描画関数を削除する
+    bpy.types.TOPBAR_MT_editor_menus.remove(LEVEL_EDITOR_MT_main_menu.submenu)
+
+    # クラスの登録解除
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
     print("レベルエディタが無効化されました。")
     
 if __name__=="__main__":
