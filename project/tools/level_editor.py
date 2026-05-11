e9660c520a343ffb881112a292d47fc1ef8229b0
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-05-11 23:43:12 +0900

    TL1_01_06

--- project/tools/level_editor.py
+++ project/tools/level_editor.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 # ブレンダーに登録するアドオン情報
 bl_info = {
@@ -32,13 +33,49 @@ class MYADDON_OT_stretch_vertex(bpy.types.Operator):
 class MYADDON_OT_create_ico_sphere(bpy.types.Operator):
     bl_idname = "myaddon.myaddon_ot_create_object"
     bl_label = "ICO球生成"
-    bl_description = "ICO球を生成します"
+    bl_description = "ICO球を生成するよ"
     bl_options = {'REGISTER', 'UNDO'}
 
     # メニューを実行したときに呼ばれる関数
     def execute(self, context):
         bpy.ops.mesh.primitive_ico_sphere_add()
         print("ICO球を生成しました。")
+
+        return {'FINISHED'}
+
+# オペレータ シーン出力
+class MYADDON_OT_export_scene(bpy.types.Operator):
+    bl_idname = "myaddon.myaddon_ot_export_scene"
+    bl_label = "シーン出力"
+    bl_description = "シーン情報をExportします"
+
+    def execute(self, context):
+
+        print("シーン情報をExportします")
+
+        # シーン内の全オブジェクトについて
+        for object in bpy.context.scene.objects:
+            print(object.type + " - " + object.name)
+            # ローカルトランスフォーム行列から平行移動、回転、スケーリングを抽出
+            # 型は Vector, Quaternion, Vector
+            trans, rot, scale = object.matrix_local.decompose()
+            # 回転を Quaternion から Euler (3軸での回転角) に変換
+            rot = rot.to_euler()
+            # ラジアンから度数法に変換
+            rot.x = math.degrees(rot.x)
+            rot.y = math.degrees(rot.y)
+            rot.z = math.degrees(rot.z)
+            # トランスフォーム情報を表示
+            print("Trans(%f, %f, %f)" % (trans.x, trans.y, trans.z))
+            print("Rot(%f, %f, %f)" % (rot.x, rot.y, rot.z))
+            print("Scale(%f, %f, %f)" % (scale.x, scale.y, scale.z))
+            # 親オブジェクトの名前を表示
+            if object.parent:
+                print("Parent:" + object.parent.name)
+            print()
+
+        print("シーン情報をExportしました")
+        self.report({'INFO'}, "シーン情報をExportしました")
 
         return {'FINISHED'}
 
@@ -49,7 +86,7 @@ class LEVEL_EDITOR_MT_main_menu(bpy.types.Menu):
     # メニューのラベルとして表示される文字列
     bl_idname = "LEVEL_EDITOR_MT_main_menu"
     # 著者表示用の文字列
-    bl_desctiption = "拡張メニュー by" + bl_info["author"]
+    bl_description = "拡張メニュー by" + bl_info["author"]
 
     # プルダウンメニューの中身
     def draw(self, context):
@@ -62,6 +99,9 @@ class LEVEL_EDITOR_MT_main_menu(bpy.types.Menu):
         layout.operator(MYADDON_OT_stretch_vertex.bl_idname, text = MYADDON_OT_stretch_vertex.bl_label)
         layout.operator(MYADDON_OT_create_ico_sphere.bl_idname, text = MYADDON_OT_create_ico_sphere.bl_label)
 
+        layout.separator()
+        layout.operator(MYADDON_OT_export_scene.bl_idname, text = MYADDON_OT_export_scene.bl_label)
+
     def submenu(self, context):
         # 自分の bl_idname を指定してメニューとして登録する
         self.layout.menu(LEVEL_EDITOR_MT_main_menu.bl_idname)
@@ -70,6 +110,7 @@ class LEVEL_EDITOR_MT_main_menu(bpy.types.Menu):
 classes = (
     MYADDON_OT_stretch_vertex,
     MYADDON_OT_create_ico_sphere,
+    MYADDON_OT_export_scene,
     LEVEL_EDITOR_MT_main_menu,
 )
 
