05564b14db4f4c297c65369828b26e9be667f846
剛 増谷 <K024G0075@g.neec.ac.jp>
2026-06-01 10:25:54 +0900

    BoxFilter, GaussianFilter修正

--- project/Resources/shader/GaussianFilter.PS.hlsl
+++ project/Resources/shader/GaussianFilter.PS.hlsl
@@ -4,6 +4,8 @@ Texture2D<float4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 
 // 定数を定義
+static const float PI = 3.14159265f;
+
 static const float2 kIndex3x3[3][3] =
 {
     { { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f } },
@@ -11,17 +13,19 @@ static const float2 kIndex3x3[3][3] =
     { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
 };
 
-static const float kKernel3x3[3][3] =
-{
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-    { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
-};
-
+// 構造体定義
 struct PixelShaderOutput
 {
     float4 color : SV_TARGET;
 };
+
+// 関数定義
+float gauss(float x, float y, float sigma)
+{
+    float exponent = -(x * x + y * y) * rcp(2.0f * sigma * sigma);
+    float denominator = 2.0f * PI * sigma * sigma;
+    return exp(exponent) * rcp(denominator);
+}
 
 PixelShaderOutput main(VertexShaderOutput input)
 {
@@ -32,17 +36,25 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     output.color.rgb = float3(0.0f, 0.0f, 0.0f);
     output.color.a = 1.0f;
+    
+    float weight = 0.0f;
+    float kernel3x3[3][3];
     for (int x = 0; x < 3; ++x)
     {
         for (int y = 0; y < 3; ++y)
         {
+            // kernelを求める
+            kernel3x3[x][y] = gauss(kIndex3x3[x][y].x, kIndex3x3[x][y].y, 2.0f);
+            weight += kernel3x3[x][y];
+            
             // 現在のtexcoordを算出
             float2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
             // 色に 1/9 掛けて足す
             float3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
-            output.color.rgb += fetchColor * kKernel3x3[x][y];
+            output.color.rgb += fetchColor * kernel3x3[x][y];
         }
+
     }
-   
+    
     return output;
 }
